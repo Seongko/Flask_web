dc93b4aa44f9d3d88662b41a7029d7e40f86818e
Seongko <hoya1398@gmail.com>
2026-05-25 12:37:27 +0900

    Update search logic

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # BLUEPRINT | DONT EDIT
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask("JobScraper")
-
 
 def load_jobs():
     with open("jobs.json", "r", encoding="utf-8") as f:
@@ -17,6 +16,8 @@ def load_jobs():
 
 # /YOUR CODE
 
+db = {}
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -25,8 +26,38 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    indeed_jobs = load_jobs()
-    return render_template("search.html", keyword = keyword, jobs = indeed_jobs)
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        results = db[keyword]
+    else:
+        indeed_jobs = load_jobs()
+        results = []
+        for job in indeed_jobs:
+            if keyword.lower() in job["title"].lower():
+                title = job["title"]
+                company = job["company_name"]
+                desc = job["description"]
+                jobs = {
+                    "title": title,
+                    "company": company,
+                    "description": desc,
+                }
+                results.append(jobs)
+
+        db[keyword] = results
+    return render_template("search.html", keyword = keyword, jobs = results)
+
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword is not db:
+        return redirect(f"/search?keyword={keyword}")
+
 
 # BLUEPRINT | DONT EDIT
 
